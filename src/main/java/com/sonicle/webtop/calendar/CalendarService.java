ef64ad4323eb714aca72be3601861d018c969431
Matteo Albinola <malbinola@sonicle.com>
2014-10-16 16:24:26 +0200

    - Initial import

---
+++ src/main/java/com/sonicle/webtop/calendar/CalendarService.java
@@ -0,0 +1,60 @@
+/*
+ * webtop-calendar is a WebTop Service developed by Sonicle S.r.l.
+ * Copyright (C) 2014 Sonicle S.r.l.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY SONICLE, SONICLE DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Sonicle S.r.l. at email address sonicle@sonicle.com
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Sonicle WebTop" logo. If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Sonicle WebTop".
+ */
+package com.sonicle.webtop.calendar;
+
+import com.sonicle.webtop.core.sdk.Environment;
+import com.sonicle.webtop.core.sdk.Service;
+import java.util.Locale;
+import org.slf4j.Logger;
+
+/**
+ *
+ * @author malbinola
+ */
+public class CalendarService extends Service {
+	
+	public static final Logger logger = Service.getLogger(CalendarService.class);
+	
+	@Override
+	public void initialize(Environment api) {
+		logger.debug("Mi sono inizializzato: mi chiamo {}", getName(new Locale("it_IT")));
+		//logger.debug("Ora provo ad accedere all'env avanzato");
+		//getAdvancedEnv().getSession().test();
+	}
+
+	@Override
+	public void cleanup() {
+		
+	}
+}

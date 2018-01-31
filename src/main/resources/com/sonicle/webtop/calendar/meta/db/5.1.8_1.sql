414ad72ab291da235f77e2e7dd0fcdc42924bbe5
Matteo Albinola <malbinola@sonicle.com>
2018-01-31 16:44:08 +0100

    === Version 5.1.8 ===

--- src/main/resources/com/sonicle/webtop/calendar/meta/db/5.1.8_1.sql
+++ src/main/resources/com/sonicle/webtop/calendar/meta/db/5.1.8_1.sql
@@ -1,4 +1,7 @@
 @DataSource[default@com.sonicle.webtop.calendar]
 
-ALTER TABLE "calendar"."events"
-ADD COLUMN "handle_invitation" bool DEFAULT false;
+-- ----------------------------
+-- Update structure for events (required in order to fix zpush invitation)
+-- ----------------------------
+ALTER TABLE "calendar"."events" ADD COLUMN "handle_invitation" bool DEFAULT false;
+CREATE INDEX "events_ak99" ON "calendar"."events" ("handle_invitation");

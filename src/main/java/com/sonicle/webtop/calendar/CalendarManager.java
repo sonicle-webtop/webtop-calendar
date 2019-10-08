cf44558e4efa0d43be2febeb8c1a65a46d42de7d
Matteo Albinola <malbinola@sonicle.com>
2019-10-08 15:48:23 +0200

    Fix wrong recurring AD event multiday length WTCALENDAR-87

--- src/main/java/com/sonicle/webtop/calendar/CalendarManager.java
+++ src/main/java/com/sonicle/webtop/calendar/CalendarManager.java
@@ -2739,6 +2739,7 @@ public class CalendarManager extends BaseManager implements ICalendarManager {
 		DateTime eventStart = instanceMapper.getEventStartDate();
 		DateTime eventEnd = instanceMapper.getEventEndDate();
 		DateTimeZone eventTimezone = instanceMapper.getEventTimezone();
+		int eventDays = CalendarUtils.calculateLengthInDays(eventStart, eventEnd);
 		LocalTime eventStartTime = eventStart.withZone(eventTimezone).toLocalTime();
 		LocalTime eventEndTime = eventEnd.withZone(eventTimezone).toLocalTime();
 		
@@ -2761,7 +2762,7 @@ public class CalendarManager extends BaseManager implements ICalendarManager {
 					if (obrecs.containsKey(recurringDate)) continue; // Skip broken date...
 
 					DateTime start = recurringDate.toDateTime(eventStartTime, eventTimezone).withZone(userTimezone);
-					DateTime end = recurringDate.toDateTime(eventEndTime, eventTimezone).withZone(userTimezone);
+					DateTime end = recurringDate.plusDays(eventDays).toDateTime(eventEndTime, eventTimezone).withZone(userTimezone);
 					String key = EventKey.buildKey(eventId, eventId, recurringDate);
 
 					instances.add(instanceMapper.createInstance(key, start, end));
@@ -2776,7 +2777,7 @@ public class CalendarManager extends BaseManager implements ICalendarManager {
 					if (obrecs.containsKey(recurringDate)) continue; // Skip broken date...
 
 					DateTime start = recurringDate.toDateTime(eventStartTime, eventTimezone).withZone(userTimezone);
-					DateTime end = recurringDate.toDateTime(eventEndTime, eventTimezone).withZone(userTimezone);
+					DateTime end = recurringDate.plusDays(eventDays).toDateTime(eventEndTime, eventTimezone).withZone(userTimezone);
 					String key = EventKey.buildKey(eventId, eventId, recurringDate);
 
 					instances.add(instanceMapper.createInstance(key, start, end));

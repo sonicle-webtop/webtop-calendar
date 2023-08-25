3a86e2044992c7e9861c2abe28a5dc751259c484
Matteo Albinola <malbinola@sonicle.com>
2023-08-25 10:18:13 +0200

    === Version 5.17.3 ===

--- src/main/resources/com/sonicle/webtop/calendar/meta/db/5.17.3_1.sql
+++ src/main/resources/com/sonicle/webtop/calendar/meta/db/5.17.3_1.sql
@@ -1,5 +1,5 @@
 @DataSource[default@com.sonicle.webtop.calendar]
 
 ALTER TABLE "calendar"."calendars_changes" 
-DROP CONSTRAINT "calendars_changes_calendar_id_fkey",
-DROP CONSTRAINT "calendars_changes_event_id_fkey";
+DROP CONSTRAINT IF EXISTS "calendars_changes_calendar_id_fkey",
+DROP CONSTRAINT IF EXISTS "calendars_changes_event_id_fkey";
